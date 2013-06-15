874a2f7675436e0c5345971387c41361f2e35718
lcwen <10889787@student.uwa.edu.au>
2013-06-15 19:23:00 +0800

    exercises done

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,11 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-  before_save { |user| user.email = email.downcase }
+  before_save { email.downcase! }
   before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
+  after_validation { self.errors.messages.delete(:password_digest) }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ SampleApp::Application.routes.draw do
   match '/about',   :to => 'static_pages#about'
   match '/contact', :to => 'static_pages#contact'
 
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
